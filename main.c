5e7c7011d56aff107bfdae422e345b2b206c972d
Rafael Jacob <105748926+RafaelJacobb@users.noreply.github.com>
2022-10-19 20:54:13 +0000

    Foi adicionado mais duas opções: Lagarto e Spock aumentando as possibilidades de jogada!

--- main.c
+++ main.c
@@ -4,16 +4,16 @@ int main(){
   do{
     do{
       printf("Escolha uma opção\n");
-      printf("1 pedra, 2 papel, 3 tesoura ou -1 para sair\n");
+      printf("1 Pedra, 2 Papel, 3 Tesoura, 4 Lagarto, 5 Spock ou -1 para sair\n");
       scanf("%d", &jogador);
 
       if(jogador == -1){
         printf("Fim de jogo");
         break;
 
-        }else if(jogador <1 || jogador > 3)
+        }else if(jogador <1 || jogador > 5)
         printf("opção inválida, tente novamente!\n\n");
-      }while(jogador < 1 || jogador > 3);
+      }while(jogador < 1 || jogador > 5);
 
     //Escolha do jogador
     
@@ -22,11 +22,15 @@ int main(){
     }
 
     if(jogador == 1)
-      printf("Você escolheu pedra.\n\n");
+      printf("Você escolheu Pedra.\n\n");
     if(jogador == 2)
-      printf("Você escolheu papel.\n\n");
+      printf("Você escolheu Papel.\n\n");
     if(jogador == 3)
-      printf("Você escolheu tesoura.\n\n");
+      printf("Você escolheu Tesoura.\n\n");
+    if(jogador == 4)
+      printf("Você escolheu Lagarto.\n\n");
+    if(jogador == 5)
+      printf("Você escolheu Spock.\n\n");
 
     //Escolha do computador
 
@@ -34,11 +38,15 @@ int main(){
      computador=("%d", rand()%3 + 1);
 
     if(computador == 1)
-      printf("O computador escolheu pedra.\n\n");
+      printf("O computador escolheu Pedra.\n\n");
     if(computador == 2)
-      printf("O computador escolheu papel.\n\n");
+      printf("O computador escolheu Papel.\n\n");
     if(computador == 3)
-      printf("O computador escolheu tesoura.\n\n");
+      printf("O computador escolheu Tesoura.\n\n");
+    if(computador == 4)
+      printf("O computador escolheu Lagarto.\n\n");
+    if(computador == 5)
+      printf("O computador escolheu Spock.\n\n");
 
     //resultado
 
@@ -51,30 +59,86 @@ int main(){
     //Jogador vence
     
     if(jogador == 1 && computador == 3){
-      printf("Você ganhou!\n\n");
+      printf("Você ganhou!\nA pedra quebrou a tesoura!\n\n");
       i = i + 1;
       }
-    if(jogador == 2 && computador ==1){
-      printf("Vcoê ganhou!\n\n");
+    if(jogador == 2 && computador == 1){
+      printf("Vcoê ganhou!\nO papel embrulhou a pedra!\n\n");
       i = i + 1;
       }
     if(jogador == 3 && computador == 2){
-      printf("Você ganhou!\n\n");
+      printf("Você ganhou!\nA tesoura cortou o papel!\n\n");
+      i = i + 1;
+      }
+    if(jogador == 1 && computador == 4){
+      printf("Você ganhou!\nA pedra esmagou o lagarto!\n\n");
+      i = i + 1;
+      }
+    if(jogador == 4 && computador == 5){
+      printf("Você ganhou!\nO lagarto envenenou o Spock!\n\n");
+      i = i + 1;
+      }
+    if(jogador == 5 && computador == 3){
+      printf("Você ganhou!\nO Spock derreteu a tesoura!\n\n");
+      i = i + 1;
+      }
+    if(jogador == 3 && computador == 4){
+      printf("Você ganhou!\nA tesoura decapitou o lagarto\n\n");
+      i = i + 1;
+      }
+    if(jogador == 4 && computador == 2){
+      printf("Você ganhou!\nO lagarto comeu o papel\n\n");
+      i = i + 1;
+      }
+    if(jogador == 2 && computador == 5){
+      printf("Você ganhou!\nO papel refutou o Spock\n\n");
+      i = i + 1;
+      }
+    if(jogador == 5 && computador == 1){
+      printf("Você ganhou!\nO Spock vaporizou a pedra\n\n");
       i = i + 1;
       }
 
       //Jogador perde
       
     if(jogador == 1 && computador == 2){
-      printf("Você perdeu!\n\n");
+      printf("Você perdeu!\nA pedra foi embrulhada pelo papel!\n\n");
       c = c +1;
       }
     if(jogador == 2 && computador == 3){
-      printf("Você perdeu!\n\n");
+      printf("Você perdeu!\nO papel foi cortado pela tesoura\n\n");
       c = c +1;
       }
     if(jogador == 3 && computador == 1){
-      printf("Você perdeu!\n\n");
+      printf("Você perdeu!\nA tesoura foi quebrada pela pedra\n\n");
+      c = c +1;
+      }
+    if(jogador == 4 && computador == 3){
+      printf("Você perdeu!\nO lagarto foi decapitado\n\n");
+      c = c +1;
+      }
+    if(jogador == 4 && computador == 1){
+      printf("Você perdeu!\nO lagarto foi esmagado\n\n");
+      c = c +1;
+      }
+    if(jogador == 5 && computador == 4){
+      printf("Você perdeu!\nO Spock foi envenenado\n\n");
+      c = c +1;
+      }
+    if(jogador == 5 && computador == 2){
+      printf("Você perdeu!\nO Spock foi refutado\n\n");
+      c = c +1;
+      }
+    if(jogador == 3 && computador == 5){
+      printf("Você perdeu!\nA tesoura foi derretida\n\n");
+      c = c +1;
+      }
+    if(jogador == 2 && computador == 4){
+      printf("Você perdeu!\nO Lagarto comeu o papel\n\n");
+      c = c +1;
+      }
+    if(jogador == 1 && computador == 5){
+      printf("Você perdeu!\nA pedra foi vaporizada\n\n");
       c = c +1;
       }
     
